314d9ec39f6a7fe3ec052ab83ed7704f2a1d8eae
李文少 <bob840312@gmail.com>
2016-07-15 18:28:34 +0800

    123

---
+++ index4.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var ObjectID = require('mongodb').ObjectID;
+var app = express();
+var md5 = require('md5');
+var gcm = require('node-gcm');
+
+var mongodbURL = 'mongodb://bob840312:bob0932233875@ds023624.mlab.com:23624/cram';
+
+var myDB;
+mongodb.MongoClient.connect(mongodbURL, function(err, db) {
+	if (err) {
+		console.log(err);
+	} else {
+		myDB = db;
+		console.log('connection success');
+	}
+});
+
+
+//將帳號、密碼、名稱存入login資料庫
+app.get('/api/insert', function(request, response) {
+	var item = {
+		user : request.query.user,
+		password : md5(request.query.password),
+		desc : request.query.desc
+	}
+	var collection = myDB.collection('login');
+	collection.insert(item, function(err, result) {
+		if (err) {
+			response.status(406).send(err).end();
+		}else {
+			response.type('application/json');
+			response.status(200).send(result).end();
+		}
+	});
+});
+
+
+//回傳密碼比對，若成功登入將UID、名稱紀錄起來
+app.get('/api/query', function(request, response) {
+
+	var collection = myDB.collection('login');
+	collection.find({account : request.query.account}, {password: 1, _id: 1, desc: 1}).toArray(function(err, docs) {
+		if (err) {
+			response.status(406).send(err).end();
+		} else {
+			response.type('application/json');
+			response.status(200).send(docs).end();
+		}
+	});
+}); 
+
+
+app.listen(process.env.PORT || 5000);
+console.log('port ' + (process.env.PORT || 5000));
+
+
+
